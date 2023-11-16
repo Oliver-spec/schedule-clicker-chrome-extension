837bbead0b3a89de60c8dc3daa3bc98846a7e625
Oliver <1155175373@link.cuhk.edu.hk>
2023-11-16 17:24:40 +0800

    Preselected 'enroll' button. Removed default date selection. Fix style error.

--- index.js
+++ index.js
@@ -3,23 +3,31 @@ const injectScript = () => {
     document.getElementById("time-to-click").value
   ).getTime();
   // const clickCount = document.getElementById("click-count-selector").value;
-  const targetBtnId = document.getElementById("id-selector").value;
+  // const targetBtnId = document.getElementById("id-selector").value;
 
   document.getElementById("status").innerHTML = "Time Set 😛";
 
   let clicked = false;
 
   setInterval(() => {
+    let remainingTime = (timeToClick - Date.now()) / 1000
+
+    if (!clicked) {
+      document.getElementById("timer").innerHTML = `Seconds remaining: ${remainingTime}`
+    } else {
+      document.getElementById("timer").innerHTML = "Clicked!"
+    }
+
     if (!clicked && timeToClick <= Date.now()) {
       chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.scripting.executeScript({
-          func: (id) => {
-            if (document.getElementById(id)) {
-              document.getElementById(id).click();
+          func: () => {
+            if (document.getElementById("DERIVED_SSR_FL_SSR_ENROLL_FL")) {
+              document.getElementById("DERIVED_SSR_FL_SSR_ENROLL_FL").click();
               console.log("clicked");
             }
           },
-          args: [targetBtnId],
+          // args: [targetBtnId],
           target: {
             tabId: tabs[0].id,
           },
